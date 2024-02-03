bcd55b398437d1e59ea9a0c8f3351356c3a38442
AflahAhamed00 <aflahahamed777@gmail.com>
2024-02-04 00:19:41 +0530

    Slightly changed validation

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,48 +1,49 @@
 import { body } from "express-validator";
 
-  const loginValidator = [
-    body("email")
+const loginValidator = [
+  body("email")
     .notEmpty()
     .withMessage("email should not be empty")
     .isEmail()
-    .withMessage("Invalid email"), 
-  
-    body("password", "password should not  be empty").notEmpty(),
-  ];
+    .withMessage("Invalid email"),
 
-  const signupValidator = [
-    body('userName')
+  body("password", "password should not  be empty").notEmpty(),
+];
+
+const signupValidator = [
+  body("userName")
     .notEmpty()
-    .withMessage('Name cannot be empty')
+    .withMessage("Name cannot be empty")
     .matches(/^[a-zA-Z\s]+$/)
-    .withMessage('Name should not contain numbers or special characters')
-    .isLength({min:5,max:20})
+    .withMessage("Name should not contain numbers or special characters")
+    .isLength({ min: 5, max: 20 })
     .withMessage("The user name length should be between 5-20 letters"),
-    
-    body("email")
+
+  body("email")
     .isEmail()
-    .withMessage( "Invalid email")
+    .withMessage("Invalid email")
     .notEmpty()
     .withMessage("email should not be empty"),
 
-    body('password')
+  body("password")
     .notEmpty()
-    .withMessage('Password cannot be empty')
+    .withMessage("Password cannot be empty")
     .matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/)
-    .withMessage('Password should contain at least one uppercase letter, one digit, and one special character')
+    .withMessage(
+      "Password should contain at least one uppercase letter, one digit, and one special character"
+    )
     .isLength({ min: 6 })
     .withMessage("The minimum password length is 6 characters"),
-    
-    body("phoneNumber")
+
+  body("phoneNumber")
     .notEmpty()
-    .withMessage('Phone number should not be empty')
+    .withMessage("Phone number should not be empty")
     .matches(/^\d+$/)
     .withMessage("Phone number should be digits")
-    .isLength({min:10,max:10})
+    .isLength({ min: 10, max: 10 })
     .withMessage("10 digit phone number is required")
-    .custom((value) => typeof value === 'number')
-    .withMessage('Phone number should be of type Number'),
-  ];
+    .custom((value) => typeof value === "number")
+    .withMessage("Phone number should be of type Number"),
+];
 
-
-export {loginValidator, signupValidator};
+export { loginValidator, signupValidator };
